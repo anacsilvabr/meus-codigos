ff21bcb13d697be35cf7d2b25fd5b50a146efca0
Ana Clara Brasil <anaclaradasilvabrasil8@gmail.com>
2025-10-07 15:23:26 -0300

    Create Q1OrdBus.java

---
+++ Q1OrdBus.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Q1OrdBus{
+ public static void main(String [] args){
+    Scanner sc = new Scanner(System.in); 
+    System.out.println("Digite o número a ser encontrado:  ");
+    int Nencontrado = sc.nextInt();
+    int contador = 0;
+
+    System.out.println("Digite N");
+    int valorN = sc.nextInt();
+
+    int [] v = new int [valorN];
+
+        for(int i=0; i < v.length; i++){
+            System.out.println("Digite todos os números: ");
+
+            
+        }
+ }
+}
